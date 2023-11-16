82025a8013f2a6222c6c34152a636945992f13dc
duyhxm <2251012049duy@ou.edu.vn>
2023-11-16 22:24:53 +0700

    complete m_graph.cpp file

--- m_graph.cpp
+++ m_graph.cpp
@@ -1,88 +1,90 @@
-//#include<iostream>
-//#include<vector>
-//
-//struct Edge {
-//	float w;
-//	int u;
-//	int v;
-//};
-//
-//using edge = Edge;
-//using graph = std::vector<std::vector<float>>;
-//
-//graph build_graph(int vertices)
-//{
-//	graph g(vertices, std::vector<float>(vertices,0.0));
-//
-//	return g;
-//}
-//
-//void add_edge(graph &g, edge e)
-//{
-//	if (e.u >= g.size() || e.v >= g.size() || e.u < 0 || e.v < 0)
-//		return;
-//	g[e.u][e.v] = e.w;
-//}
-//
-//void delete_edge(graph& g, edge e)
-//{
-//	if (e.u >= g.size() || e.v >= g.size() || e.u < 0 || e.v < 0)
-//		return;
-//	g[e.u][e.v] = 0.0;
-//}
-//
-//void print_graph(graph g)
-//{
-//	for (int i = 0; i < g.size(); i++)
-//	{
-//		for (int j = 0; j < g.size(); j++)
-//			std::cout << g[i][j] << "  ";
-//		std::cout << std::endl;
-//	}
-//}
-//
-//int main()
-//{
-//	graph g;
-//	int vertices=0;
-//
-//	std::cout << "Enter the number of vertices: ";
-//	std::cin >> vertices;
-//
-//	g = build_graph(vertices);
-//
-//	std::cout << "Enter your choice: " << std::endl;
-//	while (true)
-//	{
-//		std::cout << "-----------------------------" << std::endl;
-//		std::cout << "1. Adding an edge\n";
-//		std::cout << "2. Removing an edge\n";
-//		std::cout << "3. Printing\n";
-//		std::cout << "4. Exit\n";
-//		std::cout << "Typing a number to choose: ";
-//		short choice = 0;	std::cin>>choice;
-//		std::cout << "-----------------------------" << std::endl;
-//		edge e{ 0,0,0 };
-//		if (choice == 1)
-//		{
-//			std::cout << "Enter sequentially original point, destination point and weight that you need to add: ";
-//			std::cin >> e.u >> e.v >> e.w;
-//			add_edge(g, e);
-//		}
-//		else
-//			if (choice == 2)
-//			{
-//				std::cout << "Enter sequentially original point, destination point that you need to remove:  ";
-//				std::cin >> e.u >> e.v;
-//				delete_edge(g, e);
-//			}
-//			else
-//				if (choice == 3)
-//					print_graph(g);
-//				else
-//					exit(0);
-//	}
-//
-//	system("pause>0");
-//	return 0;
-//}
+#include<iostream>
+#include<vector>
+
+struct Edge {
+	float w;
+	int u;
+	int v;
+};
+
+using edge = Edge;
+using graph = std::vector<std::vector<float>>;
+
+graph build_graph(int vertices)
+{
+	graph g(vertices, std::vector<float>(vertices,0.0));
+
+	return g;
+}
+
+void add_edge(graph &g, edge e)
+{
+	if (e.u >= g.size() || e.v >= g.size() || e.u < 0 || e.v < 0)
+		return;
+	g[e.u][e.v] = e.w;
+	g[e.v][e.u] = e.w;
+}
+
+void delete_edge(graph& g, edge e)
+{
+	if (e.u >= g.size() || e.v >= g.size() || e.u < 0 || e.v < 0)
+		return;
+	g[e.u][e.v] = 0.0;
+	g[e.v][e.u] = 0.0;
+}
+
+void print_graph(graph g)
+{
+	for (int i = 0; i < g.size(); i++)
+	{
+		for (int j = 0; j < g.size(); j++)
+			std::cout << g[i][j] << "  ";
+		std::cout << std::endl;
+	}
+}
+
+int main()
+{
+	graph g;
+	int vertices=0;
+
+	std::cout << "Enter the number of vertices: ";
+	std::cin >> vertices;
+
+	g = build_graph(vertices);
+
+	std::cout << "Enter your choice: " << std::endl;
+	while (true)
+	{
+		std::cout << "-----------------------------" << std::endl;
+		std::cout << "1. Adding an edge\n";
+		std::cout << "2. Removing an edge\n";
+		std::cout << "3. Printing\n";
+		std::cout << "4. Exit\n";
+		std::cout << "Typing a number to choose: ";
+		short choice = 0;	std::cin>>choice;
+		std::cout << "-----------------------------" << std::endl;
+		edge e{ 0,0,0 };
+		if (choice == 1)
+		{
+			std::cout << "Enter sequentially original point, destination point and weight that you need to add: ";
+			std::cin >> e.u >> e.v >> e.w;
+			add_edge(g, e);
+		}
+		else
+			if (choice == 2)
+			{
+				std::cout << "Enter sequentially original point, destination point that you need to remove:  ";
+				std::cin >> e.u >> e.v;
+				delete_edge(g, e);
+			}
+			else
+				if (choice == 3)
+					print_graph(g);
+				else
+					exit(0);
+	}
+
+	system("pause>0");
+	return 0;
+}
